028c2d148bdecd9c861bb6a59234bf1b794eccea
Andrew Hu <andrewhu@cs.washington.edu>
2018-12-08 10:06:17 -0800

    =SymbolTable instead of HashMap

--- Type.hs
+++ Type.hs
@@ -4,7 +4,7 @@ import Text.ParserCombinators.ReadP
 import Control.Applicative ((<|>),)
 import Data.Char (isAlpha,isSpace,)
 
-import Data.HashMap.Strict (HashMap, empty, lookup, insert)
+import Data.HashMap.Strict (HashMap, empty,)
 import qualified Data.HashMap.Strict as HM
 
 import Parser
@@ -18,8 +18,18 @@ data Type -- Type information
   | TypeUnknown -- Just for debuggingm, a valid program should *never* have a variable with type unknown
   | TypeModule -- This also needs to contain a map
   deriving Show
---type SymbolTable = HashMap VarName Type
-data TCRes = TCSuccess (HashMap String Type) | TCFail String (HashMap String Type) deriving Show
+data SymbolTable = SymbolTable (HashMap String Type) (Maybe SymbolTable) deriving Show
+getType :: String -> SymbolTable -> Maybe Type
+getType varName (SymbolTable env parent) = case HM.lookup varName env of
+                                             Just bind -> Just bind
+                                             Nothing -> case parent of
+                                               Nothing -> Nothing
+                                               Just parent -> getType varName parent
+
+insert :: String -> Type -> SymbolTable -> SymbolTable
+insert name defType (SymbolTable env parent) = SymbolTable (HM.insert name defType env) parent
+
+data TCRes = TCSuccess SymbolTable | TCFail String SymbolTable deriving Show
 -- Automatically creates and accumulates the symbol table while traversing the AST
 runCheck :: a -> TCRes
 runCheck root = error "Not implemented yet"
@@ -27,30 +37,30 @@ runCheck root = error "Not implemented yet"
 checkBlock :: Block -> TCRes
 checkBlock block = error "Not implemented yet"
 
-checkStmnt :: Stmnt -> HashMap String Type -> TCRes
+checkStmnt :: Stmnt -> SymbolTable -> TCRes
 checkStmnt stmnt env = case stmnt of
   DeclVar name singleType -> checkDecl name singleType env
   DeclLet name singleType -> checkDecl name singleType env
   Init decl val -> checkInit decl val env
 
 -- TODO: Check that the type of the expression matches the declared type
-checkInit :: Stmnt -> Expr -> HashMap String Type -> TCRes
+checkInit :: Stmnt -> Expr -> SymbolTable -> TCRes
 checkInit decl val env = case decl of
   DeclVar name singleType -> checkDecl name singleType env
   DeclLet name singleType -> checkDecl name singleType env
   _ -> TCFail "Invalid declaration in an init statement" env
 
-checkDecl :: String -> String -> HashMap String Type -> TCRes
-checkDecl name singleType env = case HM.lookup name env of
+checkDecl :: String -> String -> SymbolTable -> TCRes
+checkDecl name singleType env = case getType name env of
                                   Nothing -> bindVar name singleType env -- TODO Fix the type being entered, it should translate the string into a type name, maybe looking it up in the environment
                                   Just entryType -> if typeEqual entryType singleType then TCSuccess env else TCFail "Subsequent variable declarations must have same type" env
   
-bindVar :: String -> String -> HashMap String Type -> TCRes
+bindVar :: String -> String -> SymbolTable -> TCRes
 bindVar name singleType env = case castToType singleType env of
                                 TypeUnknown -> TCFail ( "Could not find type " ++ singleType ++ " in scope") env
                                 typeListing -> TCSuccess (insert name typeListing env)
 
-castToType :: String -> HashMap String Type -> Type
+castToType :: String -> SymbolTable -> Type
 castToType str env
   | str == "string" = TypeString
   | str == "number" = TypeNumber
